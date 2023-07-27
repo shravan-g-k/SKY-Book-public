c51b2b18c198b25368ed492cbd1c2d6e4cdd7d91
Shravan G K <shravangkulal.programs@gmail.com>
2023-07-27 23:31:28 +0530

    Concersatinal AI added

--- server/hugging_face.mjs
+++ server/hugging_face.mjs
@@ -3,13 +3,36 @@ import { HuggingFaceAPIkey } from "./private.js";
 const hf = new HfInference(HuggingFaceAPIkey);
 
 async function AI(data) {
-  const wrap = "You are 'Helpful' and 'Kind' person You give answer to people with clear and motivating words." ;
+  const wrap =
+    "You are 'Helpful' and 'Kind' person You give answer to people with clear and motivating words.";
 
   for await (var output of hf.textGenerationStream({
     model: "OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5",
-    inputs:  (wrap + data),
+    inputs: wrap + data,
     parameters: { max_new_tokens: 500 },
-  })) {  }
-  return output.generated_text
+  })) {
+  }
+  return output.generated_text;
 }
-export default AI;
+
+async function AI2(data) {
+  var output = await hf.conversational({
+    model: "af1tang/personaGPT",
+    inputs: {
+      text: data,
+      // past_user_inputs: [
+      //   "Hi",
+      //   "Myself chetu",
+      //   "Whats your name",
+      //   "Do you have any pets?",
+      // ],
+      // generated_responses: [
+      //   "Hiya",
+      //   "I'm a girl",
+      //   "My name is samantha, what is yours? Do you have any pets?",
+      // ],
+    },
+  });
+  return output.generated_text;
+}
+export { AI, AI2 };
--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
-import AI  from "./hugging_face.mjs";
+import {AI,AI2}  from "./hugging_face.mjs";
 import express from "express";
-AI("Why is my life so sad?").then(
+// AI("Why is my life so sad?").then(
+//     (response) => {
+//         console.log(response);
+//     }
+// );
+AI2("Do you know who is the prime minister of india").then(
     (response) => {
         console.log(response);
     }
