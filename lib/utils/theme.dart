5e2137aa13d66061f8ea56eedad9dedd351c42a2
Shravan G K <shravangkulal.programs@gmail.com>
2023-07-30 12:39:53 +0530

    theme changed (using material design)

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -12,17 +12,19 @@ class MyTheme {
   ThemeMode? themeMode;
   SharedPreferences? sharedPreferences;
   late Color primaryColor;
+  late Color onPrimaryColor;
   late Color secondaryColor;
+  late Color onSecondaryColor;
   late Color backgroundColor;
-  late Color textColor;
-  late Color iconColor;
+  late Color onBackgroundColor;
 
 // We are initializing the theme mode and the colors
 // check if the user has already set the theme mode (that is stored in shared preferences)
 // if not then we set the theme mode to dark(default)
   void init(SharedPreferences pref) async {
-    sharedPreferences = pref;//setting the shared preferences
-    bool? darkMode = sharedPreferences!.getBool('darkTheme');//getting the theme mode if it is set else will be null
+    sharedPreferences = pref; //setting the shared preferences
+    bool? darkMode = sharedPreferences!.getBool(
+        'darkTheme'); //getting the theme mode if it is set else will be null
     // darkMode -> true if dark theme else false
     // If theme mode is found to be light then we set the theme mode to light
     if (darkMode != null && !(darkMode)) {
@@ -38,19 +40,29 @@ class MyTheme {
     if (theme == ThemeMode.dark) {
       themeMode = ThemeMode.dark;
       sharedPreferences!.setBool('darkTheme', true);
-      primaryColor = const Color.fromARGB(255, 0, 51, 102);
-      secondaryColor = const Color.fromARGB(255, 102, 153, 204);
-      backgroundColor = const Color.fromARGB(255, 0, 0, 0);
-      textColor = const Color.fromARGB(255, 255, 255, 255);
-      iconColor = const Color.fromARGB(255, 255, 255, 255);
+      ColorScheme colorScheme = ColorScheme.fromSeed(
+        seedColor: const Color.fromARGB(255, 89, 0, 255),
+        brightness: Brightness.dark,
+      );
+      primaryColor = colorScheme.primary;
+      onPrimaryColor = colorScheme.onPrimary;
+      secondaryColor = colorScheme.secondary;
+      onSecondaryColor = colorScheme.onSecondary;
+      backgroundColor = colorScheme.background;
+      onBackgroundColor = colorScheme.onBackground;
     } else {
       themeMode = ThemeMode.light;
       sharedPreferences!.setBool('darkTheme', false);
-      primaryColor = const Color.fromARGB(255, 0, 123, 255);
-      secondaryColor = const Color.fromARGB(255, 76, 175, 79);
-      backgroundColor = const Color.fromARGB(255, 255, 255, 255);
-      textColor = const Color.fromARGB(255, 51, 51, 51);
-      iconColor = const Color.fromARGB(255, 0, 0, 0);
+      ColorScheme colorScheme = ColorScheme.fromSeed(
+        seedColor: const Color.fromARGB(255, 89, 0, 255),
+        brightness: Brightness.light,
+      );
+      primaryColor = colorScheme.primary;
+      onPrimaryColor = colorScheme.onPrimary;
+      secondaryColor = colorScheme.secondary;
+      onSecondaryColor = colorScheme.onSecondary;
+      backgroundColor = colorScheme.background;
+      onBackgroundColor = colorScheme.onBackground;
     }
   }
 
