d83f1403ef3c3827504e0e87a040bddead566a99
Shravan G K <shravangkulal.programs@gmail.com>
2023-08-06 19:49:32 +0530

    routes.dart moved to utils folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:journalbot/routes.dart';
+import 'package:journalbot/utils/routes.dart';
 
 void main() async {
   runApp(
---
+++ lib/utils/routes.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:journalbot/screens/auth_screen.dart';
+import 'package:journalbot/screens/home_screens/home_screen.dart';
+import 'package:journalbot/wrapper.dart';
+
+class MyRouter {
+  // All the routes in the app
+  static const homeRoute = '/home';
+  static const authRoute = '/auth';
+  // 
+
+  // The router config
+  static final routerConfig = GoRouter(
+    routes: [
+      // Initial route '/' - Wrapper
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const Wrapper(),
+      ),
+      // Home route - HomeScreen
+      GoRoute(
+        name: homeRoute,
+        path: homeRoute,
+        pageBuilder: (context, state) =>
+            customTransitionPage(const HomeScreen(), key: state.pageKey),
+      ),
+      // Auth route - AuthScreen
+      GoRoute(
+        name: authRoute,
+        path: authRoute,
+        pageBuilder: (context, state) {
+          return customTransitionPage(const AuthScreen(), key: state.pageKey);
+        },
+      ),
+    ],
+  );
+  // CUstom transition used for all the routes
+  static CustomTransitionPage customTransitionPage(Widget child,
+      {required LocalKey key}) {
+    return CustomTransitionPage(
+      key: key,
+      child: child,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        return FadeTransition(
+          opacity: animation,
+          child: child,
+        );
+      },
+    );
+  }
+}
