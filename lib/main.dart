19a221ffe28d2c5d2e649185cee97427c75342d5
Shravan G K <shravangkulal.programs@gmail.com>
2023-08-10 01:54:14 +0530

    completely changed the theme now not using riverpod for theme management

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:journalbot/utils/routes.dart';
+import 'package:journalbot/utils/theme.dart';
 
 void main() async {
   runApp(
@@ -13,13 +14,36 @@ void main() async {
 }
 
 // TODO : Change the name of the class
-class JournalBot extends StatelessWidget {
+class JournalBot extends ConsumerWidget {
   const JournalBot({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final brightness = ref.watch(brightnessNotifierProvider);
     return MaterialApp.router(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 89, 0, 255),
+          brightness: brightness,
+        ),
+        textTheme: const TextTheme(
+          bodyLarge: titleTextStyle,
+          bodyMedium: titleTextStyle,
+          bodySmall: titleTextStyle,
+          headlineLarge: titleTextStyle,
+          headlineMedium: titleTextStyle,
+          headlineSmall: titleTextStyle,
+          displayLarge: titleTextStyle,
+          displayMedium: titleTextStyle,
+          displaySmall: titleTextStyle,
+          labelLarge: titleTextStyle,
+          labelMedium: titleTextStyle,
+          labelSmall: titleTextStyle,
+          
+        )
+      ),
+
       routerConfig: MyRouter.routerConfig,
       title: 'Journal Bot',
     );
--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -1,79 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-final themeProvider = StateProvider((ref) {
-  return MyTheme();
+final brightnessNotifierProvider =
+    StateNotifierProvider<BrightnessNotifier, Brightness>((ref) {
+  return BrightnessNotifier();
 });
 
-// All the themes Dark and Light are defined here
-// Used shared preferences to store the theme mode
-class MyTheme {
-  ThemeMode? themeMode;
-  SharedPreferences? sharedPreferences;
-  late Color primaryColor;
-  late Color onPrimaryColor;
-  late Color secondaryColor;
-  late Color onSecondaryColor;
-  late Color backgroundColor;
-  late Color onBackgroundColor;
+class BrightnessNotifier extends StateNotifier<Brightness> {
+  BrightnessNotifier() : super(Brightness.dark);
 
-// We are initializing the theme mode and the colors
-// check if the user has already set the theme mode (that is stored in shared preferences)
-// if not then we set the theme mode to dark(default)
-  void init(SharedPreferences pref) async {
-    sharedPreferences = pref; //setting the shared preferences
-    bool? darkMode = sharedPreferences!.getBool(
-        'darkTheme'); //getting the theme mode if it is set else will be null
-    // darkMode -> true if dark theme else false
-    // If theme mode is found to be light then we set the theme mode to light
-    if (darkMode != null && !(darkMode)) {
-      _setTheme(ThemeMode.light);
-    } else {
-      // Else in all case ie no theme mode found or it is dark already
-      _setTheme(ThemeMode.dark);
-    }
-  }
-
-// Used by this class only to set the theme mode and the colors
-  void _setTheme(ThemeMode theme) {
-    if (theme == ThemeMode.dark) {
-      themeMode = ThemeMode.dark;
-      sharedPreferences!.setBool('darkTheme', true);
-      ColorScheme colorScheme = ColorScheme.fromSeed(
-        seedColor: const Color.fromARGB(255, 89, 0, 255),
-        brightness: Brightness.dark,
-      );
-      primaryColor = colorScheme.primary;
-      onPrimaryColor = colorScheme.onPrimary;
-      secondaryColor = colorScheme.secondary;
-      onSecondaryColor = colorScheme.onSecondary;
-      backgroundColor = colorScheme.background;
-      onBackgroundColor = colorScheme.onBackground;
-    } else {
-      themeMode = ThemeMode.light;
-      sharedPreferences!.setBool('darkTheme', false);
-      ColorScheme colorScheme = ColorScheme.fromSeed(
-        seedColor: const Color.fromARGB(255, 89, 0, 255),
-        brightness: Brightness.light,
-      );
-      primaryColor = colorScheme.primary;
-      onPrimaryColor = colorScheme.onPrimary;
-      secondaryColor = colorScheme.secondary;
-      onSecondaryColor = colorScheme.onSecondary;
-      backgroundColor = colorScheme.background;
-      onBackgroundColor = colorScheme.onBackground;
-    }
-  }
-
-// Used By the UI to toggle the theme mode
-// Calls the _setTheme() to set the theme mode
-// If the theme mode is dark then we set it to light and vice versa
-  void toggleTheme() {
-    if (themeMode == ThemeMode.dark) {
-      _setTheme(ThemeMode.light);
-    } else {
-      _setTheme(ThemeMode.dark);
-    }
+  void toggle() {
+    state = state == Brightness.light ? Brightness.dark : Brightness.light;
   }
 }
+
+const TextStyle titleTextStyle = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w300,
+);
