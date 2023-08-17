349178cbba45c290d968ed5333f10d79f27d8753
Shravan G K <shravangkulal.programs@gmail.com>
2023-08-18 00:45:52 +0530

    format doc and fix a bug

--- server/routes/page.js
+++ server/routes/page.js
@@ -12,15 +12,15 @@ const pageRouter = Express.Router();
 pageRouter.post("/page/create", auth, async (req, res) => {
   try {
     // encoded is the encrypted string
-    const { encoded,bookId } = req.body;
+    const { encoded, bookId } = req.body;
     // create a new page
     const page = new Page({ encoded });
     // save the page
     const doc = await page.save();
     // add the page to the book
-    Book.findByIdAndUpdate(bookId, { $push: { pages: doc._id } });
+    await Book.findByIdAndUpdate(bookId, { $push: { pages: doc._id } });
     // send the page
-    res.status(200).json({ doc });
+    res.status(200).json(doc);
   } catch (error) {
     res.status(400).json({ msg: "Error creating page" });
   }
