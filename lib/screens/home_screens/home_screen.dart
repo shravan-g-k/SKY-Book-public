1e5b3e292b1f6a29f93c9ac40a798098f63c3ad7
Shravan G K <shravangkulal.programs@gmail.com>
2023-08-27 16:10:40 +0530

    removed comma from home_screen greeting

--- lib/screens/home_screens/home_screen.dart
+++ lib/screens/home_screens/home_screen.dart
@@ -34,12 +34,12 @@ class _HomeScreenState extends ConsumerState<HomeScreen>
   String wishUser() {
     var hour = DateTime.now().hour;
     if (hour < 12) {
-      return "Good Morning,";
+      return "Good Morning";
     }
     if (hour < 17) {
-      return "Good Afternoon,";
+      return "Good Afternoon";
     }
-    return "Good Evening,";
+    return "Good Evening";
   }
 
   @override
