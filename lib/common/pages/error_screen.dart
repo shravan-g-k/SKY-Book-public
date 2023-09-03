5e823225cd93ce72d6b25b043652a86cea14c171
Shravan G K <shravangkulal.programs@gmail.com>
2023-09-03 21:43:49 +0530

    tt

--- lib/common/pages/error_screen.dart
+++ lib/common/pages/error_screen.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 class MyErrorWidget extends StatelessWidget {
   const MyErrorWidget({super.key});
-
+// 
+// 
+// 
   @override
   Widget build(BuildContext context) {
     return const Center(
