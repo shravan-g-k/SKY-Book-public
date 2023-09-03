14a058468556eb0b756811871f249d138fc1a820
Shravan G K <shravangkulal.programs@gmail.com>
2023-09-03 21:40:09 +0530

    secret files

---
+++ server/private.js
@@ -0,0 +1,4 @@
+const jwtPasswordKey = "123456";
+const mongoDBpassword = "nZynEEm0v7GzTFpU";
+const palmAPIkey = "AIzaSyDT - tmHHHQhBCR9rWSWXX6rqKPlRYAnlRs";
+export { jwtPasswordKey, mongoDBpassword,palmAPIkey };
