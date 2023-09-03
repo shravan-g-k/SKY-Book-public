d3767718cceaa513b88d17cdc8ac92d761f40a9d
Shravan G K <shravangkulal.programs@gmail.com>
2023-09-03 21:36:45 +0530

    test

--- lib/const.dart
+++ lib/const.dart
@@ -2,5 +2,5 @@ import 'package:journalbot/private.dart';
 // Used to get the server address deployed on localhost
 // ipv4 is the IP address of the machine running the server(my computer)
 const serverAddress = "http://$ipv4:3000";
-// 
+
 const String tokenKey = "token";
