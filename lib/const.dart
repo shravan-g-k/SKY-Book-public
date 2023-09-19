7ab703d482fb199c94d7eab088182ac5baac14cf
Shravan G K <shravangkulal.programs@gmail.com>
2023-09-19 21:11:47 +0530

    render link

--- lib/const.dart
+++ lib/const.dart
@@ -1,5 +1,5 @@
 // Render server address
-const serverAddress = "http://192.168.178.154:3000";
+const serverAddress = "https://skybook-15p0.onrender.com";
 
 const String tokenKey = "token";
 const String darkModeKey = 'darkMode';
